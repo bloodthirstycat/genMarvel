b28793d292d5dd8793d048c8d774171f014991c5
BadOmen94PL <46285576+BadOmen94PL@users.noreply.github.com>
2018-12-31 19:22:29 +0100

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,328 @@
+var bohaterowie_male = [
+"Iron Man",
+"Punisher",
+"DareDevil",
+"Iron Fist",
+"Kapitan Ameryka",
+"Ghost Rider",
+"Thor",
+"Hulk",
+"Spider Man",
+"Luke Cage",
+"Wolverine",
+"Czarna Pantera",
+"Doktor Strange",
+"DeadPool",
+"DeathLok",
+"Warlock",
+"Hawkeye",
+"Ludzka Pochodnia",
+"Nick Fury",
+"Blade",
+"Star Lord",
+"Szop Rocket",
+"Nova",
+"Bucky",
+"Vision",
+"Ant-Man",
+"Cyklops",
+"Mr Fantastic"
+];
+
+var bohaterowie_female = [
+"Kapitan Marvel",
+"Mocking Bird",
+"Spider Gwen",
+"Gwen Pool",
+"Wasp",
+"Gwen Stancy",
+"Gamora",
+"Spider Girl",
+"Czarna Wdowa",
+"Jessica Jones",
+"She Hulk",
+"Dagger",
+"Mary Jane",
+"Jean Grey",
+"Biały Tygrys",
+"Walkiria",
+"Karen Page",
+"Vanessa",
+"HellCat",
+"Hela",
+"Elektra"
+];
+
+var evilman = [
+"Kingpin",
+"Thanos",
+"Dr Doom",
+"Profesor X",
+"Venom",
+"Ichabod",
+"Purple Man",
+"Red Skull",
+"Sabertooth",
+"Foggy Nelson",
+"Loki",
+"Juggernut",
+"Green Goblin",
+"Norman Osborn",
+"Killmonger",
+"Ronan Oskarżyciel",
+"Ultron",
+"J. Jonah Jameson",
+"Mefisto",
+"Janusz Tracz"
+];
+
+var aktorzy_male = [
+"Mikołaj Roznerski",
+"Krzysztof Czeczot",
+"Paweł Domagała",
+"Łukasz Garlicki",
+"Jan Frycz",
+"Tomasz Kot",
+"Piotr Adamczyk",
+"Wojciech Mecwaldowski",
+"Tomasz Karolak",
+"Maciej Stuhr",
+"Borys Szyc",
+"Zbigniew Zamachowski",
+"Rafał Królikowski",
+"Michał Koterski",
+"Maciej Zakościelny",
+"Paweł Małaszyński",
+"Rafał Cieszyński",
+"Mateusz Damięcki",
+"Wojciech Solarz",
+"Jakub Wesołowski",
+"Rafał Rutkowski",
+"Piotr Głowacki",
+"Dariusz Kowalski"
+];
+
+var aktorzy_female = [
+"Weronika Rosati",
+"Barbara Kurdej-Szatan",
+"Anna Dereszowska",
+"Danuta Stenka",
+"Katarzyna Bujakiewicz",
+"Sonia Bohosiewicz",
+"Małgorzata Kożuchowska",
+"Agnieszka Dygant",
+"Magdalena Różczka",
+"Anna Dereszowska",
+"Marta Żmuda Trzebiatowska",
+"Edyta Olszówka",
+"Małgorzata Socha",
+"Katarzyna Zielińska",
+"Roma Gąsiorowska",
+"Izabela Kuna",
+"Olga Bołądź",
+"Agnieszka Więdłocha"
+];
+
+var zawod = [
+"dołącza się do X-men ",
+"ma problemy emocjonalne",
+"ma problem ze swoją płcią",
+"został lewakiem",
+"zaczął robić karierę rapu z Eminmem",
+"naucza w kościele Latającego Potwora Spaghetti",
+"zostaje prezydentem Polski",
+"został żebrakiem",
+"ma problemy psychiczne",
+"rozmawiał z rysownikiem Marvela, i go pobił",
+"skończył szkołę marketingu",
+"został programistą baz danych",
+"skoczył z mostu",
+"pracuje w cyrku",
+"zbudował autobota",
+"rozmawia z kosmitami",
+"zgwałcił Skrulla",
+"nasrał do zbroi",
+"nasikał sobie do kostiumu",
+"rozdaje ulotki",
+"zwątpił w swój cel życiowy",
+"zastanawia się, czemu nigdy nie spotkał Batmana",
+"dołączył do Teen Titans",
+"zniszczył mienie publiczne",
+"został polskim kabareciarzem",
+"ściga przestępców podatkowych",
+"poucza w szkole",
+"kłócił się z fanem DC",
+"zdaje sesję",
+"szuka pracy",
+"pracuje jako sprzątacz",
+"stał się kasjerem biedronki",
+"złamał kość ogonową",
+"tworzy memy",
+"opiekuje się kotami",
+"pracuje w pralni",
+"wyemigrował z Europy",
+"rysuje komiksy o swoich przygodach, jednak dostaje wypowiedzenie z pracy od Marvela",
+"zaczął nagrywać swoje utwory Disco Polo",
+"programuje w Javie"
+];
+
+var kto = [
+"połowa Nowego Jorku",
+"pieniądz",
+"goście odpowiedzialni za MCU",
+"koleżanka",
+"jego kostium",
+"rząd USA",
+"były Prezydent USA",
+"ludzie od IT",
+"jej ex-przyjacióła",
+"jego ręka",
+"Hitler",
+"pies",
+"kot",
+"chłopak",
+"babcia",
+"dziadek",
+"chomik",
+"brat",
+"Daily Bugle",
+"Marvel",
+"kuzyn",
+"ich sąsiad",
+"ex"
+];
+
+var co_z_kobieta = [
+"sprzedaje w warzywniaku",
+"oddała się za pół darmo",
+"próbowała owniąć go wokół palca",
+"przepaliła pierogi",
+"przesoliła zupę",
+"zapłaciła podatek w naturze",
+"przebłagała chłopaka, żeby kupił jej działkę",
+"hoduje podejrzane kwiatki",
+"zdradziła go z sąsiadem",
+"poznała sekretną tożsamość jej chłopaka na Tinderze",
+"przytyła o 20 kg",
+"występuje jako aktorka porno",
+"prowadzi burdel",
+"zalega z czynszem",
+"nie chciała spełnić jego chorych zachcianek",
+"zaczeła kupować komiksy z DC"
+];
+
+var co_sie_dzieje = [
+"zdradza ją z jej najlepszą koleżanką",
+"on ginie w wyniku eksplozji atomowej",
+"on ginie potrącony przez samochód",
+"ginie w niewyjaśnionych okolicznościach",
+"zostaje porwany przez UFO",
+"był kosmitą",
+"strzela focha",
+"przestaje się do niej odzywać",
+"posiada zdolności paranormalne",
+"posiada zdolności magiczne",
+"posiada zdolność rozmawiania ze zwierzętami",
+"posiada zdolność latania",
+"posiada kamień nieskończoności w swojej dupie",
+"przechodzi na stronę zła",
+"dostaje misję uratowania wszechświata",
+"zostaje wystrzelony na marsa i raczej nie wróci",
+"zostaje wysłany na misję samobójczą",
+"znajduje portal prowadzący do uniwersum DC",
+"trafia na wojownika z innego wymiaru"
+];
+
+var co_postanawia = [
+"rzucić pracę",
+"poznać nowego faceta",
+"udać się do psychiatry",
+"nauczyć się czegoś nowego",
+"skoczyć z wieżowca",
+"wyjechać za granicę",
+"nauczyć się karate",
+"zebrać kamienie nieskończoności",
+"zostać mnichem tybetańskim",
+"wspiąć się na Mount Everest",
+"nauczyć się gotować",
+"nauczyć się tańczyć",
+"skoczyć na bungee",
+"wyruszyć na podróż by go / ją odszukać",
+"się zemścić",
+"znaleźć nową pracę",
+"adoptować zwierzaka",
+"zniszczyć wszechswiat",
+"spędzać więcej czasu z rodziną",
+"spędzić noc z jego sąsiadem",
+"zrobić remont w domu",
+"znaleźć innego faceta, póki może",
+"ugotować pierogi",
+"zadzwnonić do teściowej",
+"upić się do rana",
+"rzucić ten związek i spędzić dalsze życie z kotami"
+];
+
+var a_co_z_wrogiem = [
+"przeciera oczy ze zdziwienia",
+"przeprasza za II Wojnę Światową",
+"prowadzi program na TVN24",
+"trzyma kciuki za ich związek",
+"wypuł kawę gdy o tym usłyszał",
+"otrzymał zaproszenie na ich ślub",
+"prowadzi stronę internetową przeciwko im",
+"włamał się na ich konto na facebooku",
+"przekonał Netflixa do stworzenia serialu o tym",
+"puknął się w głowę",
+"strzelił sobie w łeb",
+"stracił nadzieję na romans",
+"powiadomił o tym służby",
+"podał ich do sądu",
+"był jej poprzednim kochankiem",
+"napisał hejty na ich kanale youtube",
+"został ich lokajem",
+"nie wie, że jest ojcem jej przyszego dziecka",
+"wstał i zaczął bić im brawo",
+"podstawił im pod drzwi stado kotów, coś przy tym sugerując...",
+"czasami odwiedza ją w nocy"
+];
+
+var ale_czy_co = [
+"ich związek przetrwa próbę czasu",
+"Warszawa obroni się przed hordą zmutowanych zombie",
+"wyborcy zagłosują na zmiennokształtnego jaszczura w ciele człowieka",
+"meteory zniszczą ziemię",
+"dzięki temu DC Universe zacznie mieć lepsze wyniki finansowe",
+"dostaną pieniądze z 500+, gdy mają wyższe zarobki",
+"Peter Parker przestanie być ciamajdą",
+"Polacy dostaną się do Mistrzostw Świata",
+"walcząc z demonami dowiedzą się prawdy o sobie",
+"pokonają zło, jakie ciąży nad ziemią",
+"Polska przetrwa atomową apokalipsę"
+];
+
+function losuj(tablica){
+	return tablica[Math.floor(Math.random()*tablica.length)];
+}
+
+function odmien(imie){
+	if(imie == "Paweł"){
+		return "Pawła";
+	}else if(imie == "Antoni"){
+		return "Antoniego";
+	}else if(imie == "Ksawery"){
+		return "Ksawerego";
+	}else if(imie == "Kacper"){
+		return "Kacpra";
+	}else if(imie == "Ignacy"){
+		return "Ignacego";
+	}else if(imie == "Franciszek"){
+		return "Franciszka";
+	}else{
+		return imie + "a";
+	}
+}
+
+function generuj(){
+	var zdanie = losuj(bohaterowie_male) + " (" + losuj(aktorzy_male) + ") " +  losuj(zawod) + " i próbuje stworzyć związek z " + losuj(bohaterowie_female) + " (" + losuj(aktorzy_female) + ")" + ", która " + losuj(co_z_kobieta) + ". Niestety, okazuje się, że ten " + losuj(co_sie_dzieje) + ", więc ona postanawia " + losuj(co_postanawia) + ". Tymczasem " + losuj(evilman) + " (" + losuj(aktorzy_male) + ") " + losuj(a_co_z_wrogiem) +". Ale czy " + losuj(ale_czy_co) + "?";
+	document.getElementById("gen").innerText = zdanie;
+}
